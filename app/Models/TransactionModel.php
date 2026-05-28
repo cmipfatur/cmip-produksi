e10ec815dfdc1516ab7fb174bcd45cc750813897
syahrulizha <74164767+SyahrulIM@users.noreply.github.com>
2026-05-28 14:24:45 +0700

    update models

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PreProcessTransactionModel extends Model
+class TransactionModel extends Model
 {
     use HasFactory;
 }
